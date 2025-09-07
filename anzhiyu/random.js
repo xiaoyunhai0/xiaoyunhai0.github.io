c66a8ee63061113da5943bd3ec27414979c5ec7b
xiaoyunhai0 <2849409442@qq.com>
2025-09-07 23:51:10 +0800

    Site updated: 2025-09-07 23:51:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/17/hello-world/","2025/08/17/xLua-热更入门与实践指南/"];function toRandomPost(){
+var posts=["2025/08/07/Flappy-Bird-XLua-Migration/","2025/07/22/Lua & xLua 语法教学（Unity向）/","2025/07/03/Hello-Hexo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
