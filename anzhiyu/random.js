59ea19fbe2ec39e53bd7de220f683e1afba84977
xiaoyunhai0 <2849409442@qq.com>
2025-11-03 15:34:47 +0800

    Site updated: 2025-11-03 15:34:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/Flappy-Bird-XLua-Migration/","2025/07/03/Hello-Hexo/","2025/08/08/3DRacing/","2025/07/22/Lua & xLua 语法教学（Unity向）/","2025/08/22/Git使用SSH的Push和Pull/","2025/08/20/ARSandBox/"];function toRandomPost(){
+var posts=["2025/08/07/Flappy-Bird-XLua-Migration/","2025/07/03/Hello-Hexo/","2025/08/08/3DRacing/","2025/07/22/Lua & xLua 语法教学（Unity向）/","2025/08/22/Git使用SSH的Push和Pull/","2025/08/20/ARSandBox/","2025/10/01/unity常用工程目录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
