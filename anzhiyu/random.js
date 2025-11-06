3f45477bd524bec67dabd0c7aa3b73053db98827
xiaoyunhai0 <2849409442@qq.com>
2025-11-06 16:18:42 +0800

    Site updated: 2025-11-06 16:18:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/Flappy-Bird-XLua-Migration/","2025/07/03/Hello-Hexo/","2025/08/08/3DRacing/","2025/07/22/Lua & xLua 语法教学（Unity向）/","2025/08/22/Git使用SSH的Push和Pull/","2025/08/20/ARSandBox/","2025/10/03/Unity常用插件/","2025/10/10/Unity_Framework/","2025/10/01/Unity常用工程目录/","2025/11/03/Komari探针/","2025/11/01/Linux常用命令行/"];function toRandomPost(){
+var posts=["2025/08/07/Flappy-Bird-XLua-Migration/","2025/07/03/Hello-Hexo/","2025/08/08/3DRacing/","2025/07/22/Lua & xLua 语法教学（Unity向）/","2025/08/22/Git使用SSH的Push和Pull/","2025/08/20/ARSandBox/","2025/10/03/Unity常用插件/","2025/10/10/Unity_Framework/","2025/10/01/Unity常用工程目录/","2025/11/03/Komari探针/","2025/11/01/Linux常用命令行/","2025/11/05/VPS操作手册/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
